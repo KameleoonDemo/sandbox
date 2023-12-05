cdc159ba8c0bae15dc3d2db79761edb06f8ad7dc
unknown <jdoujet@gmail.com>
2023-12-05 17:40:03 +0100

    sitecode

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 //import { KameleoonUtils } from '../node_modules/@kameleoon/javascript-sdk/dist/index.js';
 const { KameleoonUtils } = KameleoonSDK;
 // -- Constants values
-export const siteCode = "44r9jgmatl";
+export const siteCode = "7qo2upjb8p";
 export const featureKeys = ["demo_web_feature_flag_carousel", "demo_web_feature_flag_blocks", "demo_web_feature_flag_banner"];
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
